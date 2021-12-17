ab5683d27e5915a5e3ef00215316b95b27078417
Adam <adamszydlo@gmail.com>
2021-12-17 23:23:21 +0100

    added /bin/bash

--- ansible-lab/post-install.sh
+++ ansible-lab/post-install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 apt-get update && \
 apt-get install -y gcc vim python-dev libkrb5-dev iproute2 atop htop lsof ftp lftp mc curl iputils-ping net-tools traceroute openssh-server ruby && \
 apt-get install python3-pip -y && \
